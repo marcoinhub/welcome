4a88b15fefa0aefc12475ca8354b81cad15b9b72
刁文杰 <diaowenjie@Jie-Pro.local>
2015-08-11 15:11:49 +0800

    Initial Commit

---
+++ welcome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  welcome
+//
+//  Created by 刁文杰 on 15/8/11.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ welcome/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  welcome
+//
+//  Created by 刁文杰 on 15/8/11.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
